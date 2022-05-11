849fa7720a745ad9e358b497f23e326b08ece7dd
rlawlstjd13 <104603814+rlawlstjd13@users.noreply.github.com>
2022-05-11 15:11:01 +0900

    Add files via upload

--- trn02/js/main.js
+++ trn02/js/main.js
@@ -40,6 +40,37 @@ $(function () {
     });
 
 
+    $('.product_slider').slick({
+        slidesToShow: 5,
+        centerMode: true,
+        arrows: false,
+        dots: true,
+        autoPlay: true,
+    });
+
+    $('.product_list .s_left').on('click', function () {
+        $('.product_slider').slick('slickPrev');
+    });
+
+    $('.product_list .s_right').on('click', function () {
+        $('.product_slider').slick('slickNext');
+    });
+
+
+
+    $('.tab_link>li').on('click', function () {
+        var idx = $(this).index();
+        $(this).addClass('on').siblings().removeClass('on');
+        $('.tab_content>li').eq(idx).addClass('on').siblings().removeClass('on');
+    });
+
+    $('.footer #link').on('change', function () {
+        var lik = $(this).val();
+        if (lik) window.open(lik)
+    });
+
+
+
 
 
 
